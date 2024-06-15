bbf2b5713fc838ee3ec03fa158ccd5ea6083aff9
Tejas <tejas.bajania@gmail.com>
2024-06-15 13:48:50 +0530

    Added archive process

--- Assignments/archive_data.sh
+++ Assignments/archive_data.sh
@@ -1,30 +1,35 @@
 #!/bin/bash
 
-# List entire directory
-# Loop over directory with file
+for files in /home/tejas/test/*.*; do
 
-    for files in /home/tejas/Downloads/*.*; do
+    file_size_prev=`du -b $files`
+    file_size_new=`du -b $files | cut -f1` # 100 MB = 104857600 Bytes
 
-        file_size_prev=`du -b $files`
-        file_size_new=`ls -lh $files`
+    if [ $file_size_new -gt 104857600 ]; then
 
-        echo "\n"
-        echo "$files"
-        echo "$file_size" 
-        echo "$file_size_new"
+        echo $files is greater than 100 MB
 
-    done
-
-    # Check if file size is greater than 100 MB 
-
-        # Check timestamp if it is older than 3 days
-
-            # If yes
-
-                # Delete it
+        # collect both times in seconds-since-the-epoch
+        three_days_ago=$(date -d 'now - 3 days' +%s)
         
-            # If no
 
-                # Archive
-                    # Compress file
-                    # Move to archive location
+        file_time=$(date -r "$files" +%s)
+        
+        if (( $file_time < $three_days_ago));then
+            
+            echo $files was modified less than 3 days ago  with $file_time please compress and archive it
+            time_folder_name=$(date +%Y%m%d)
+            mkdir -p /home/tejas/test/archive/"$foldername"
+            mv $files  /home/tejas/test/archive/"$foldername"
+
+            echo "Files archived"
+
+        else
+            mkdir -p /home/tejas/test/delete/"$foldername"
+            mv $files  /home/tejas/test/delete/"$foldername"
+            echo $files was modified more than 3 days ago  with $file_time please delete it
+            echo "Files deleted"
+        fi
+    fi
+    
+done
